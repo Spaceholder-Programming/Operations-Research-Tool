85715d439db09d749826def2331589a8feef5dda
SinusFox <61253950+SinusFox@users.noreply.github.com>
2024-09-24 17:39:06 +0200

    Setting up Unit Testing using jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+/** @type {import('jest').Config} */
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+};
+
+module.exports = createJestConfig(customJestConfig);
